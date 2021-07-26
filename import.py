866a01531f81104fbd86679129925a318419a3f2
Matt Conroy <matthew.conroy@topcashback.co.uk>
2021-07-26 12:45:04 +0100

    add profiles

--- import.py
+++ import.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 import argparse
+import configparser
 import messages.core
 import messages.facebook
 import messages.whatsapp
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--profile", help="the name of the import profile to use")
 parser.add_argument("--facebook", help="the Facebook-specific name of the messages to import")
 parser.add_argument("--whatsapp", help="the WhatsApp-specific name of the messages to import")
 args = parser.parse_args()
+
+if args.profile:
+    config = configparser.ConfigParser()
+    config.read('profiles.ini')
+    if args.profile in config: 
+        args.facebook = config[args.profile]['Facebook']
+        args.whatsapp = config[args.profile]['WhatsApp']
+    else:
+        raise ValueError(f'Could not find profile named \'{args.profile}\'.')
 
 if args.facebook is None and args.whatsapp is None:
     print('No arguments specified. Using defaults.')
